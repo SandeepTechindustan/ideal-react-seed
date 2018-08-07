73c457a72a15ee92d0dd176bcae64ac94d88289c
Sandeep Rajput <sandeep.techindustan@gmail.com>
2018-08-07 13:48:34 +0530

    Added Images

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ class App extends Component {
         <Grid item xs={12} sm={12} md={12}>
           <h1>This is a test</h1>
         </Grid>
+        <Grid item xs={12} sm={12} md={12}>
+          <img src="./images/sample1.jpg" width="80%" />
+          <img src="./images/sample2.jpg" width="80%" />
+          <img src="./images/sample3.jpg" width="80%" />
+          <img src="./images/sample4.jpg" width="80%" />
+        </Grid>
         <Grid item xs={12} sm={12} md={12} style={{position: 'fixed', bottom: 0, textAlign: 'center'}}>
           <h2>By: Sandeep</h2>
         </Grid>
