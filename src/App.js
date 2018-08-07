6c4cada84af007e81a320ccd562353e6b8018c9c
Sandeep Rajput <sandeep.techindustan@gmail.com>
2018-08-07 11:55:14 +0530

    added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Ideal-React-Seed</h1>
-        </header>
-        <p className="App-intro">
-          My Name is Sandeep
-        </p>
+      <div>
+        <h1>Hello Dhruv MehndiRatta</h1>
       </div>
     );
   }
