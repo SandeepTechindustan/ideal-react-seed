f34e3b39183eb309be430b0b41ddfd0ab07e834a
Sandeep Rajput <sandeep.techindustan@gmail.com>
2018-08-07 13:16:10 +0530

    Added Material-UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello Dhruv MehndiRatta</h1>
-      </div>
+      <Grid container style={{fontFamily: 'ubuntu', textAlign: 'center'}}>
+        <Grid item xs={12} sm={12} md={12}>
+          <h1>This is a test</h1>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} style={{position: 'fixed', bottom: 0, textAlign: 'center'}}>
+          <h2>By: Sandeep</h2>
+        </Grid>
+      </Grid>
     );
   }
 }
