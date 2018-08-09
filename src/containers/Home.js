bd6a435b70ed6d19566ce0be6a18978de86140c9
Sandeep Rajput <sandeep.techindustan@gmail.com>
2018-08-09 11:35:50 +0530

    Replaced config prod

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,25 +9,7 @@ export default class Home extends React.Component {
   render() {
     
     return(
-      <Container className="home-container">
-        <Item xs={12} sm={6} className="left-side">
-          <Container>
-            <Item xs={12} className="fixed-panel">
-              {home.name}
-            </Item>
-          </Container>
-        </Item>
-        <Item xs={12} sm={6} className="right-side">
-          <Container>
-            <Item xs={12}>
-              {home.bio.map((i, n) => <p key={n}>{i}</p>)}
-            </Item>
-            <Item xs={12}>
-              
-            </Item>
-          </Container>
-        </Item>
-      </Container>
+      <h1>Hello Sandeep</h1>
     )
   }
 }
